21ce5238a800d6f60a6c0740953d64160fea95da
etaeleith <sabas.ll.w@gmail.com>
2023-04-22 00:36:06 -0300

    0.0.2c - Clear bs comments

--- src/components/Main/Tweet/InterfaceTweet.jsx
+++ src/components/Main/Tweet/InterfaceTweet.jsx
@@ -39,33 +39,3 @@ const InterfaceTweet = () => {
 };
 
 export default InterfaceTweet;
-{
-  /* <div className={styles.div}>
-  <div className={styles.textModule}>
-    <div className={styles.button}>
-      <span className={styles.span}>Everyone</span>
-      <img
-        className={styles.img}
-        src="https://icongr.am/fontawesome/arrow-down.svg?size=14&color=1DA1F2"
-      />
-    </div>
-    <form onSubmit={handleSubmit}>
-      <textarea
-        className={styles.input}
-        placeholder="What's happening?"
-        style={{height: `${tweet.split("\n").length + 1}em`}}
-        value={tweet}
-        onChange={handleChange}
-      />
-    </form>
-  </div>
-  <div className={styles.borderBottom}>
-    <div className={styles.canReply}>
-      <img src={world} />
-      <div className={styles.textReply}>Everyone can reply</div>
-    </div>
-  </div>
-  <TweetComposer count={tweet.length} limit={280} />
-</div>;
- */
-}
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import path from "path";
 
 import react from "@vitejs/plugin-react-swc";
 import {defineConfig} from "vite";
-// https://vitejs.dev/config/
+
 export default defineConfig({
   resolve: {
     alias: {
